646013645bc3a7ef9aafbcf3ed2e14f0346ab823
Linhdx <vn.doxuan.linh@ignitionpoint-inc.com>
2022-07-10 18:20:19 +0700

    first commit

---
+++ src/main/java/com/example/spingweb/SpingwebApplication.java
@@ -0,0 +1,13 @@
+package com.example.spingweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpingwebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpingwebApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/spingweb/SpingwebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.spingweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpingwebApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
